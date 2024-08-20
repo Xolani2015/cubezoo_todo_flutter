1fa3b2a3312a9c4b98858d7dd4228f3219f64a1e
xolani-pilot <xolani-pilot@xolani-pilots-MacBook-Pro.local>
2024-08-20 13:10:45 +0200

    feat: dev on login page

--- cubezoo_mobile_app/lib/pages/login_page.dart
+++ cubezoo_mobile_app/lib/pages/login_page.dart
@@ -21,7 +21,7 @@ class _LoginPageState extends State<LoginPage> {
     setState(() {
       emailError = EmailValidator.validate(emailController.text.trim())
           ? null
-          : 'Invalid userEmail address';
+          : 'Invalid email address';
       passwordError =
           passwordController.text.isEmpty ? 'Password is required' : null;
     });
@@ -30,8 +30,8 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final double mediaSize = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(title: Text('Login Page')),
       body: BlocListener<AuthenticationBloc, AuthState>(
         listener: (context, state) {
           if (state is LoginSuccess) {
@@ -45,55 +45,157 @@ class _LoginPageState extends State<LoginPage> {
             );
           }
         },
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
           child: Column(
             children: [
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  errorText: emailError,
-                ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: const Color.fromARGB(255, 233, 233, 233),
+                      child: Column(
+                        children: [
+                          Container(
+                            height: mediaSize * 0.1,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(
+                                  margin:
+                                      EdgeInsets.only(right: mediaSize * 0.03),
+                                  width: mediaSize * 0.05,
+                                  child: Image.asset(
+                                    'assets/images/icon.png', // Path to your image
+                                    fit: BoxFit.cover, // Adjust as needed
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: mediaSize * 0.35,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                    flex: 2,
+                                    child: Container(
+                                      margin: EdgeInsets.only(
+                                          right: mediaSize * 0.03,
+                                          left: mediaSize * 0.03),
+                                      child: Image.asset(
+                                        'assets/images/lion.png', // Path to your image
+                                        fit: BoxFit.cover, // Adjust as needed
+                                      ),
+                                    )),
+                                Expanded(
+                                    child: Container(
+                                  margin: EdgeInsets.only(
+                                    right: mediaSize * 0.04,
+                                  ),
+                                  child: Image.asset(
+                                    'assets/images/words.png', // Path to your image
+                                    fit: BoxFit.cover, // Adjust as needed
+                                  ),
+                                ))
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: mediaSize * 0.08),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
               ),
-              TextField(
-                controller: passwordController,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  errorText: passwordError,
-                ),
-                obscureText: true,
-              ),
-              SizedBox(height: 20),
-              BlocBuilder<AuthenticationBloc, AuthState>(
-                builder: (context, state) {
-                  if (state is ReqLoading) {
-                    return ElevatedButton(
-                      onPressed: null,
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  return ElevatedButton(
-                    onPressed: () {
-                      if (validateInputs()) {
-                        context.read<AuthenticationBloc>().add(
-                              reqLogin(
-                                emailController.text.trim(),
-                                passwordController.text.trim(),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: mediaSize * 0.03, horizontal: mediaSize * 0.08),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: mediaSize * 0.02),
+                    Row(
+                      children: [
+                        Text(
+                          'Login',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: mediaSize * 0.04,
+                              color: const Color.fromARGB(255, 238, 129, 129)),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: mediaSize * 0.02),
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        errorText: emailError,
+                      ),
+                    ),
+                    TextField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        errorText: passwordError,
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: mediaSize * 0.08),
+                    BlocBuilder<AuthenticationBloc, AuthState>(
+                      builder: (context, state) {
+                        if (state is ReqLoading) {
+                          return ElevatedButton(
+                            onPressed: null,
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        return Container(
+                          width: 200, // Width of the button
+                          height: 50, // Height of the button
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(
+                                255, 238, 129, 129), // Background color
+                            borderRadius:
+                                BorderRadius.circular(8), // Rounded corners
+                          ),
+                          child: InkWell(
+                            onTap: () {
+                              if (validateInputs()) {
+                                context.read<AuthenticationBloc>().add(
+                                      reqLogin(
+                                        emailController.text.trim(),
+                                        passwordController.text.trim(),
+                                      ),
+                                    );
+                              }
+                            }, // Action to be executed on tap
+                            child: Center(
+                              child: Text(
+                                'Login',
+                                style: TextStyle(
+                                  color: Colors.white, // Text color
+                                  fontSize: 16, // Text size
+                                ),
                               ),
-                            );
-                      }
-                    },
-                    child: Text('Login'),
-                  );
-                },
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    SizedBox(height: mediaSize * 0.03),
+                  ],
+                ),
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  context.read<AuthenticationBloc>().add(reqLogout());
-                },
-                child: Text('Logout'),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: const Color.fromARGB(255, 221, 221, 221),
+                      height: mediaSize * 0.07,
+                    ),
+                  )
+                ],
               ),
             ],
           ),
