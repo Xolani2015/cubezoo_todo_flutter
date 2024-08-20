96b45d582bc55cf236e483c8d3495b99551cb64f
xolani-pilot <xolani-pilot@xolani-pilots-MacBook-Pro.local>
2024-08-20 11:10:24 +0200

    add to map function

--- cubezoo_mobile_app/lib/models/todo_model.dart
+++ cubezoo_mobile_app/lib/models/todo_model.dart
@@ -13,12 +13,25 @@ class ToDo {
     required this.createdAt,
   });
 
+  // Convert ToDo object to Firestore format (Map)
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'isCompleted': isCompleted,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
   // Convert Firestore document to ToDo object
   factory ToDo.fromMap(Map<String, dynamic> map, String documentId) {
     return ToDo(
       id: documentId,
       title: map['title'],
       description: map['description'],
+      isCompleted: map['isCompleted'] ?? false,
+      createdAt: DateTime.parse(map['createdAt']),
     );
   }
 }
