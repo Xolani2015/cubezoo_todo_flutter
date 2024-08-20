c00e6522f4e74f657967dfb077500bb27d1f64cf
xolani-pilot <xolani-pilot@xolani-pilots-MacBook-Pro.local>
2024-08-20 11:22:17 +0200

    develop fetch to do functionality

--- cubezoo_mobile_app/lib/blocs/todo_bloc/todo_bloc.dart
+++ cubezoo_mobile_app/lib/blocs/todo_bloc/todo_bloc.dart
@@ -3,16 +3,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ToDoBloc extends Bloc<ToDoEvent, ToDoState> {
   final FirestoreService _firestoreService;
+  String userEmail; // Change to mutable
 
   ToDoBloc(this._firestoreService, this.userEmail) : super(ToDoInitial()) {
     on<FetchToDos>(_onFetchToDos);
     on<AddToDo>(_onAddToDo);
 
+    // Optionally handle email update
     on<UpdateUserEmail>(_onUpdateUserEmail);
   }
 
   void _onFetchToDos(FetchToDos event, Emitter<ToDoState> emit) async {
     try {
+      emit(ToDoLoading());
+      final toDos = await _firestoreService
+          .getToDos(userEmail)
+          .first; // Pass userEmail to filter
       emit(ToDoLoaded(toDos));
     } catch (e) {
       emit(const ToDoError('Failed to fetch ToDos'));
@@ -22,6 +28,7 @@ class ToDoBloc extends Bloc<ToDoEvent, ToDoState> {
   void _onDeleteToDo(DeleteToDo event, Emitter<ToDoState> emit) async {
     try {
       await _firestoreService.deleteToDo(event.id);
+      add(FetchToDos()); // Re-fetch ToDos after deleting
     } catch (e) {
       emit(const ToDoError('Failed to delete ToDo'));
     }
