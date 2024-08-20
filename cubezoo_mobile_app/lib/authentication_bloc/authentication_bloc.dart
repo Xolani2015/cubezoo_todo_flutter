f3d1fe7c4d9500b589463c9da6cd20436a0e8d2a
xolani-pilot <xolani-pilot@xolani-pilots-MacBook-Pro.local>
2024-08-20 11:13:23 +0200

    add error handling for auth bloc

--- cubezoo_mobile_app/lib/authentication_bloc/authentication_bloc.dart
+++ cubezoo_mobile_app/lib/authentication_bloc/authentication_bloc.dart
@@ -6,10 +6,39 @@ class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthState> {
   AuthenticationBloc() : super(PostAuthentication()) {
     on<reqLogin>((event, emit) async {
       emit(ReqLoading());
-      try {} catch (e) {}
+      try {} catch (e) {
+        print('Error during login: ${e.toString()}');
+        String errorMessage;
+        if (e is FirebaseAuthException) {
+          switch (e.code) {
+            case 'invalid-userEmail':
+              errorMessage = 'The userEmail address is badly formatted.';
+              break;
+            case 'user-disabled':
+              errorMessage =
+                  'The user account has been disabled by an administrator.';
+              break;
+            case 'invalid-credential':
+              errorMessage =
+                  'There is no user record corresponding to this identifier.';
+              break;
+            case 'wrong-userPassword':
+              errorMessage =
+                  'The userPassword is invalid or the user does not have a userPassword.';
+              break;
+            default:
+              errorMessage = 'An undefined error happened.';
+          }
+        } else {
+          errorMessage = 'An unexpected error occurred.';
+        }
+        print('Error message: $errorMessage');
+        emit(LoginError(errorMessage));
+      }
     });
 
     on<reqLogout>((event, emit) async {
+      await _firebaseAuth.signOut();
       emit(NotLogedin());
     });
   }
