1ebc6de0ad89bc618b8ad59f13855677edec628f
xolani-pilot <xolani-pilot@xolani-pilots-MacBook-Pro.local>
2024-08-20 11:00:33 +0200

    develop initial to do page

---
+++ cubezoo_mobile_app/lib/pages/to_do_page.dart
@@ -0,0 +1,120 @@
+import 'package:cubezoo_mobile_app/authentication_bloc/authentication_bloc.dart';
+import 'package:cubezoo_mobile_app/authentication_bloc/authentication_event.dart';
+import 'package:cubezoo_mobile_app/pages/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String searchQuery = '';
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _logout() {
+    context.read<AuthenticationBloc>().add(reqLogout());
+
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+      (Route<dynamic> route) => false,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: TextField(
+            controller: searchController,
+            decoration: const InputDecoration(
+              hintText: 'Search bar',
+              border: InputBorder.none,
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+              hintStyle: TextStyle(color: Color.fromARGB(179, 152, 147, 147)),
+            ),
+            style: const TextStyle(color: Color.fromARGB(255, 174, 174, 174)),
+            onChanged: (query) {
+              setState(() {
+                searchQuery = query;
+              });
+            },
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              _logout();
+            },
+          ),
+        ],
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddDialog(context);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _showAddDialog(BuildContext context) {
+    final titleController = TextEditingController();
+    final descriptionController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Add ToDo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: titleController,
+                decoration: InputDecoration(labelText: 'Title'),
+              ),
+              TextField(
+                controller: descriptionController,
+                decoration: InputDecoration(labelText: 'Description'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                final title = titleController.text;
+                final description = descriptionController.text;
+                if (title.isNotEmpty && description.isNotEmpty) {
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Add'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
